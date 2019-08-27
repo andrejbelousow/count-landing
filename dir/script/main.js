87212e13832d663c6ad030af4767cafcba0c1d3c
At Work <andr.belousow2017@yandex.ru>
2019-08-27 15:27:22 +0300

    Сделал рандомайзер, разделил все по функциям

--- dir/script/main.js
+++ dir/script/main.js
@@ -1,61 +1,68 @@
-// Появление текста в хедере
-let text = document.querySelectorAll(".additional-info__text"),
-    button = document.querySelector(".additional-info-buttom");
+// Переменные
+let inputs = document.querySelectorAll("input"),
+  inputsArray = Array.from(inputs),
+  randomInputs = inputsArray.slice(0, 2),
+  timerInputs = inputsArray.slice(2, 5),
+  buttons = document.querySelectorAll("button"),
+  warning = document.querySelectorAll(".warning"),
+  randomWarning = warning[0],
+  timerWarning = warning[1];
 
-setTimeout(function(){
-    text[0].style.opacity = 1;
-}, 1000, setTimeout(function() {
-    text[1].style.opacity = 1;
-    button.style.opacity = 1;
-}, 2000));
-// Появление текста в хедере end
+// Появление текста в хедере
+function fadeIn() {
+    let text = document.querySelectorAll(".additional-info__text"),
+        button = document.querySelector(".additional-info-buttom");
+    setTimeout(function(){
+      text[0].style.opacity = 1;
+      }, 1000, setTimeout(function() {
+        text[1].style.opacity = 1;
+        button.style.opacity = 1;
+        }, 2000));
+}
+
+fadeIn();
 
 // Плавный переход по якорям
-const anchors = [].slice.call(document.querySelectorAll('a[href*="#"]')),
-      animationTime = 300,
-      framesCount = 80;
+function Anchore() {
+  const anchors = [].slice.call(document.querySelectorAll('a[href*="#"]')),
+  animationTime = 300,
+  framesCount = 80;
 
-      anchors.forEach(function(item) {
-        item.addEventListener('click', function(e) {
+  anchors.forEach(function(item) {
+    item.addEventListener('click', function(e) {
 
-          e.preventDefault();
-          
-          let coordY = document.querySelector(item.getAttribute('href')).getBoundingClientRect().top;
-          
-          let scroller = setInterval(function() {
+      e.preventDefault();
+      
+      let coordY = document.querySelector(item.getAttribute('href')).getBoundingClientRect().top;
+      
+      let scroller = setInterval(function() {
 
-            let scrollBy = coordY / framesCount;
-            
-            if(scrollBy > window.pageYOffset - coordY && window.innerHeight + window.pageYOffset < document.body.offsetHeight) {
-    
-              window.scrollBy(0, scrollBy);
-            } else {
-    
-              window.scrollTo(0, coordY);
-              clearInterval(scroller);
-            }
+        let scrollBy = coordY / framesCount;
+        
+        if(scrollBy > window.pageYOffset - coordY && window.innerHeight + window.pageYOffset < document.body.offsetHeight) {
 
-          }, animationTime / framesCount);
-        });
-      });
-// Плавный переход по якорям end
+          window.scrollBy(0, scrollBy);
+        } else {
+
+          window.scrollTo(0, coordY);
+          clearInterval(scroller);
+        }
+
+      }, animationTime / framesCount);
+    });
+  });
+}
+
+Anchore();
 
 // Сброс инпутов и проверка на числовые значения
+function checkInputs() {
 
-let inputs = document.querySelectorAll("input"),
-    inputsArray = Array.from(inputs),
-    randomInputs = inputsArray.slice(0, 2),
-    timerInputs = inputsArray.slice(2, 5),
-    buttons = document.querySelectorAll("button"),
-    warning = document.querySelectorAll(".warning"),
-    randomWarning = warning[0],
-    timerWarning = warning[1];
-
-      // У рандомайза
-    randomInputs.forEach(function(item) {
+  function Checking(inputs, button, warning) {
+    inputs.forEach(function(item) {
       item.value = '';
       if(item.value == '') {
-        buttons[0].disabled = "true";
+        button.disabled = "true";
       } 
       item.onfocus = function() {
         item.classList.add("focus");
@@ -64,11 +71,10 @@ let inputs = document.querySelectorAll("input"),
         item.classList.remove("focus");
       };
       item.addEventListener("input", function() {
-        a = parseInt(item.value, 10);
-        if ( isNaN(a) && item.value != '') {
-            buttons[0].disabled = "true";
+        if ( isNaN(item) && item.value != '') {
+          button.disabled = "true";
             item.classList.add("inc-focus");
-            randomWarning.textContent = "Введите числовое значение";
+            warning.textContent = "Введите числовое значение";
             item.onfocus = function() {
               item.classList.remove("inc-blur");
               item.classList.add("inc-focus");
@@ -79,8 +85,8 @@ let inputs = document.querySelectorAll("input"),
             };
         } else if(item.value == '') {
             item.classList.add("inc-focus");
-            buttons[0].disabled = "true";
-            randomWarning.textContent = "Не оставляйте поле пустым";
+            button.disabled = "true";
+            warning.textContent = "Не оставляйте поле пустым";
             item.onfocus = function() {
               item.classList.remove("inc-blur");
               item.classList.add("inc-focus");
@@ -98,83 +104,41 @@ let inputs = document.querySelectorAll("input"),
             item.onblur = function() {
               item.classList.remove("focus");
             };
-            buttons[0].disabled = "";
-            randomWarning.textContent = "";
+            button.disabled = "";
+            warning.textContent = "";
         }
       }); 
     });
-      
-    // У таймера
-    timerInputs.forEach(function(item) {
-      item.value = '';
-      if(item.value == '') {
-        buttons[1].disabled = "true";
-      } 
-      item.onfocus = function() {
-        item.classList.add("focus");
-      };
-      item.onblur = function() {
-        item.classList.remove("focus");
-      };
-      item.addEventListener("input", function() {
-        a = parseInt(item.value, 10);
-        if ( isNaN(a) && item.value != '') {
-            buttons[1].disabled = "true";
-            item.classList.add("inc-focus");
-            timerWarning.textContent = "Введите числовое значение";
-            item.onfocus = function() {
-              item.classList.remove("inc-blur");
-              item.classList.add("inc-focus");
-            };
-            item.onblur = function() {
-              item.classList.remove("inc-focus");
-              item.classList.add("inc-blur");
-            };
-        } else if(item.value == '') {
-            item.classList.add("inc-focus");
-            buttons[1].disabled = "true";
-            timerWarning.textContent = "Не оставляйте поле пустым";
-            item.onfocus = function() {
-              item.classList.remove("inc-blur");
-              item.classList.add("inc-focus");
-            };
-            item.onblur = function() {
-              item.classList.remove("inc-focus");
-              item.classList.add("inc-blur");
-            };
-        } else {
-          item.classList.remove("inc-focus");
-          item.classList.add("focus");
-            item.onfocus = function() {
-              item.classList.add("focus");
-            };
-            item.onblur = function() {
-              item.classList.remove("focus");
-            };
-            buttons[1].disabled = "";
-            timerWarning.textContent = "";
-        }
-      }); 
-    });
+  }
 
-// Сброс инпутов и проверка на числовые значения end
-  
+  Checking(randomInputs, buttons[0], randomWarning);
+  Checking(timerInputs, buttons[1], timerWarning);
+}
 
-// Работа рандомайзера     
-    buttons[0].addEventListener("click", function() {
-        
-    });
-// Работа рандомайзера end
+checkInputs();
+
+// Работа рандомайзера  
+function Random() {
+  buttons[0].addEventListener("click", function() {
+    let f = parseInt(randomInputs[0].value),
+        g = parseInt(randomInputs[1].value),
+        randomResult = document.querySelector(".random-result");
+        a = Math.floor(Math.random() * (g - f) + f);
+    randomResult.textContent = a;
+    randomResult.style.display = "block";
+  });
+} 
+
+Random();
 
 // Работа таймера
-    buttons[1].addEventListener("click", Timer);
-
-    function Timer() {
+function Timer() {
+    buttons[1].addEventListener("click", function() {
       let timeSet = new Date(),
           inputsTimer = document.querySelectorAll(".timer-setting input");
           inputsTimer.forEach(function (item) {
             if (item.value == '') {
-              item.value = 0;
+              item.value = 00;
             }
           });
       let a = parseInt(inputsTimer[0].value, 10),
@@ -241,5 +205,7 @@ let inputs = document.querySelectorAll("input"),
         }
       }
       setClock(timeSet);
+    });  
     }
-// Работа таймера end
+
+Timer();
